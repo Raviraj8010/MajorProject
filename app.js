8f7ecc7f106482f505d9e7b9988353379f860cc4
Raviraj Kadam <ravirajskadam2004@gmail.com>
2025-07-08 03:27:52 +0530

    files updated

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const { listingSchema } = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
 const Review = require('./models/review.js');
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
@@ -37,6 +37,16 @@ app.get("/", (req, res) => {
 
 const validateListing = (req, res, next) => {
     let { error } = listingSchema.validate(req.body);
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+};
+
+const validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
     if (error) {
         let errMsg = error.details.map((el) => el.message).join(",");
         throw new ExpressError(400, errMsg);
@@ -114,21 +124,24 @@ app.delete(
 
 //Reviews
 //Post Route
-app.post("/listings/:id/reviews", async(req, res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
+app.post(
+    "/listings/:id/reviews", 
+    validateReview, 
+    wrapAsync(async(req, res) => {
+        let listing = await Listing.findById(req.params.id);
+        let newReview = new Review(req.body.review);
 
-    listing.review.push(newReview);
+        listing.review.push(newReview);
 
-    await newReview.save();
-    await listing.save();
+        await newReview.save();
+        await listing.save();
 
-    console.log("new review saved");
-    res.send("new review saved");
-});
+        res.redirect(`/listings/${ listing._id }`);
+    })
+);
 
 
-// app.get("/testListing", async (req, res) => {
+// app.get("/testListing", async (req, res) => 
 //     let sampleListing = new Listing({
 //         title: "My New Villa",
 //         description: "By the beech",
