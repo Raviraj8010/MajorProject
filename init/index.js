a170bf58d00bfdc440e70e8c7573e4f9fd9a211e
Raviraj Kadam <ravirajskadam2004@gmail.com>
2025-07-13 00:00:21 +0530

    Authorisation step updated

--- init/index.js
+++ init/index.js
@@ -18,6 +18,10 @@ async function main() {
 
 const initDB = async () => {
     await Listing.deleteMany({});
+    initData.data = initData.data.map((obj) => ({
+        ...obj, 
+        owner: "6872127dae079124c2a668ed",
+    }))
     await Listing.insertMany(initData.data);
     console.log("data was initialized");
 };
