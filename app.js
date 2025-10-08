f88025af047051e37a8f7ae68de469f030843716
Raviraj Kadam <ravirajskadam2004@gmail.com>
2025-10-08 16:43:41 +0530

    server file is updated

--- app.js
+++ app.js
@@ -109,9 +109,9 @@ app.use((err, req, res, next) => {
     // res.status(statusCode).send(message);
 });
 
-const port = process.env.PORT || 8080;
-app.listen(8080, () => {
-    console.log("server is listening to port 8080");
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
 
 
